23b15bd7eac51b0fd3f756503916fa9ef72e8f6f
sakhutale <sachin.khutale@copart.com>
2022-12-13 00:36:18 +0530

    [+][Initial commit]

---
+++ src/components/home/Social.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const social = () => {
+  return (
+    <div className='home_social'>
+        <a href='https://www.instagram.com/' className='home_social-icon' target='_blank'>
+        <i class="uil uil-instagram"></i>
+        </a>
+        <a href='https://dribbble.com/' className='home_social-icon' target='_blank'>
+        <i class="uil uil-dribbble"></i>
+        </a>
+        <a href='https://github.com/' className='home_social-icon' target='_blank'>
+        <i class="uil uil-github"></i>
+        </a>
+    </div>
+  )
+}
+
+export default social
