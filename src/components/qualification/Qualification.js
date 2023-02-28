3b0e23baeeee73df98f0c5c6d97fb2d8190fff25
sakhutale <sachin.khutale@copart.com>
2023-02-28 13:06:33 +0530

    [+][Added logic for compare screen]

---
+++ src/components/qualification/Qualification.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './qualification.css'
+
+const Qualification = () => {
+  return (
+    <section className='qualification section' id='qualification'>
+        <h2 className='section_title'>Qualification</h2>
+        <span className='section_subtitle'>My Personal Journey</span>
+
+    </section>
+  )
+}
+
+export default Qualification
